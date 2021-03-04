0145383ce939aea80e084f00554cfa4dc9374efb
ozzie-eu <mail@ozzie.eu>
2021-03-04 15:34:45 +0000

    compress folder simple program example

---
+++ compress-folder-zip/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace compress_folder_zip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+                Console.WriteLine("Usage: dotnet run <path to folder> <zip archive name>");
+            else 
+            {
+                string originPath = args[0];
+                string zipName = args[1];
+                
+                ZipFile.CreateFromDirectory(originPath, zipName);
+            }
+        }
+    }
+}
