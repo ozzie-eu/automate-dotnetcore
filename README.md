77d039f0d93f8820dfd25179f440523842517b01
João Osório <joao.osorio@CNPD.PT>
2021-03-04 17:52:32 +0000

    folder flattening program.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ This is a collection of simple programs, using C#, for automating everyday tasks
 So far I've made available examples on how to:
 - Organize files inside a folder according to date: date-named-folders
 - Compress files inside a given folder into a ZIP archive: compress-folder-zip
+- Flatten a folder, moving files to parent directory: flatten-folder
 
 
 The samples are built with [VSCode](https://github.com/microsoft/vscode), the integrated terminal and the following extensions:
---
+++ flatten-folder/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace flatten_folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2) 
+            {
+
+                string[] fileNames = Directory.GetFiles(args[0], args[1]);
+
+                // Display all files.
+                Console.WriteLine("--- Files: ---");
+                foreach (string file in Directory.EnumerateFiles(args[0], args[1],SearchOption.AllDirectories))
+                {                    
+                    //Mind if the file exists to not overwrite different files with the same name
+                    string newName = Path.GetFileName(file);
+                    int i=1;
+                    while(File.Exists(Path.Combine(args[0],newName))) 
+                    {
+                        newName = i.ToString() + Path.GetFileName(file); 
+                        i++;
+                    }
+
+                    //Move the file
+                    Console.WriteLine($"Moving file {file} to folder {args[0]}");
+                    File.Move(file,Path.Combine(args[0],newName));
+                }
+            } 
+            else 
+            {
+                Console.WriteLine("Usage: dotnet run <path to folder> <file selector>");
+            }
+        }
+    }
+}
