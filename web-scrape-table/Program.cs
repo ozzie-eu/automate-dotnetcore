557473fe2679df0816e317434de9d4c885846e7a
João Osório <joao.osorio@CNPD.PT>
2021-03-10 19:17:17 +0000

    Scrape html table to csv text file.

---
+++ web-scrape-table/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.IO;
+using System.Collections.Generic;
+
+namespace web_scrape_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://www.tiobe.com/tiobe-index/";
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load(url);
+
+            HtmlNode table = doc.DocumentNode.SelectSingleNode("//*[@id='top20']");
+
+            var rows = table.Descendants("tr");
+
+            List<string> lines = new List<string>();
+
+            foreach (HtmlNode tr in table.Descendants("tr"))
+            {
+                string line = "";
+
+                foreach(var node in tr.ChildNodes)
+                {
+                    if (node.Name != "td")
+                        continue;
+            
+                    line += node.InnerText +";";
+                }
+                lines.Add(line);
+            }
+
+            File.WriteAllLines("tiobe.csv", lines);
+        }
+    }
+}
