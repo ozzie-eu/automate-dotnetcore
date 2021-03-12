35d82f0d9e6b12bf6369dd9e68b59583052704a1
João Osório <joao.osorio@CNPD.PT>
2021-03-12 15:59:56 +0000

    Convert a CSV file to Excel format.

---
+++ save-csv-to-excel/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ClosedXML.Excel;
+
+namespace save_csv_to_excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) 
+            {
+                Console.WriteLine("usage: dotnet run <target CSV file>");
+            } 
+            else
+            {
+                string[] lines = System.IO.File.ReadAllLines(args[0]);
+
+                IXLWorkbook workbook = new XLWorkbook();
+                IXLWorksheet worksheet = workbook.Worksheets.Add("CSV Data");
+
+                for (int i = 1; i <= lines.Length; i++)
+                {
+                    string line = lines[i-1];
+                    string[] data = line.Split(';');
+
+                    for (int j = 1; j <= data.Length; j++) 
+                    {
+                        worksheet.Cell(i,j).Value = data[j-1];
+                    }
+                }
+
+                workbook.SaveAs("import.xlsx");
+            }
+            
+        }
+    }
+}
