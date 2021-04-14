e05c54465b08703a00b358d7954e10bcaafb1c94
João Osório <joao.osorio@CNPD.PT>
2021-04-14 16:58:41 +0100

    Read a JSON settings file.

---
+++ read-settings-file/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace read_settings_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Going to display the setting on apsettings.json:");
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true);
+
+            var config = builder.Build();
+            Console.WriteLine($"Hello Setting: {config["HelloSetting"]}");
+            Console.WriteLine($"Email Property Setting: {config["SettingProperties:mail"]}");
+        }
+    }
+}
